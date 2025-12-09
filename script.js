063aedb90719f62917e839dcd3d21c912e9d7f0a
shashank4521 <140044911+shashank4521@users.noreply.github.com>
2025-12-10 01:31:32 +0530

    Add styles for 'today' class in script.js
    Added CSS styles for the 'today' class.

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 // Script disabled to prevent timetable overwrite
 console.log("Timetable is now controlled only by HTML.");
+.today {
+  background: #b9ffd0 !important;
+  font-weight: bold;
+  box-shadow: inset 0 0 0 3px #00b84a;
+}
 
 
